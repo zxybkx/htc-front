ddb7786d769c58463fd7b3fb2be8d1ce2d38fe9c
zxy <xinyan.zhou@hand-china.com>
2022-08-17 11:01:39 +0800

    申请单、订单行增加列

--- packages/htc-front-iop/src/pages/invoice-req/stores/InvoiceReqListDS.ts
+++ packages/htc-front-iop/src/pages/invoice-req/stores/InvoiceReqListDS.ts
@@ -318,6 +318,16 @@ export default (): DataSetProps => {
         label: intl.get('hiop.invoiceReq.modal.failedQuantity').d('失败数量'),
         type: FieldType.number,
       },
+      {
+        name: 'systemCode',
+        label: intl.get('hiop.invoiceReq.modal.systemCode').d('来源系统'),
+        type: FieldType.string,
+      },
+      {
+        name: 'documentTypeCode',
+        label: intl.get('hiop.invoiceReq.modal.documentTypeCode').d('来源单据类型'),
+        type: FieldType.string,
+      },
     ],
     queryDataSet: new DataSet({
       events: {
