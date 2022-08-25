0fb49201eb4273394952730d304b07045ea4ebe3
yuhuishan <huishan.yu@hand-china.com>
2022-08-25 09:55:06 +0800

    loadConfig引入修改

--- packages/htc-front-ivp/src/routes-config.tsx
+++ packages/htc-front-ivp/src/routes-config.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { configure } from 'choerodon-ui';
-import { loadConfig } from '@hzerojs/plugin-hzero-layout/browsers/clients/utils/c7nUiConfig';
+import { loadConfig } from 'hzero-front/lib/utils/c7nUiConfig';
 
 configure({
   // tooltipPlacement: 'bottomRight',
@@ -8,7 +8,7 @@ configure({
 });
 
 if (process.env.ADDITIONAL === 'true') {
-  loadConfig(configure);
+  loadConfig();
 }
 export const PageWrapper = () => {
   return ({ children }: any) => {
