ac998c0fce90857124abf76ce69d938fe73f38a1
zxy <xinyan.zhou@hand-china.com>
2022-11-22 10:36:34 +0800

    勾选认证多选Lov修复

--- packages/htc-front-ivp/src/pages/check-certification/stores/NotDeductCheckDS.ts
+++ packages/htc-front-ivp/src/pages/check-certification/stores/NotDeductCheckDS.ts
@@ -468,17 +468,19 @@ export default (): DataSetProps => {
           type: FieldType.object,
           lovCode: 'HTC.SOURCE_SYSTEM',
           lovPara: { enabledFlag: 1 },
-          multiple: ',',
+          multiple: true,
           ignore: FieldIgnore.always,
         },
         {
           name: 'systemCode',
           type: FieldType.string,
+          multiple: ',',
           bind: 'systemCodeObj.systemCode',
         },
         {
           name: 'docTypeHeaderId',
           type: FieldType.string,
+          multiple: ',',
           bind: 'systemCodeObj.docTypeHeaderId',
         },
         {
@@ -500,22 +502,25 @@ export default (): DataSetProps => {
               return !record.get('docTypeHeaderId').length;
             },
           },
-          multiple: ',',
+          multiple: true,
           ignore: FieldIgnore.always,
         },
         {
           name: 'documentTypeCode',
           type: FieldType.string,
+          multiple: ',',
           bind: 'documentTypeCodeObj.documentTypeCode',
         },
         {
           name: 'docTypeLineId',
           type: FieldType.string,
+          multiple: ',',
           bind: 'documentTypeCodeObj.docTypeLineId',
         },
         {
           name: 'docuTypeHeaderId',
           type: FieldType.string,
+          multiple: ',',
           bind: 'documentTypeCodeObj.docTypeHeaderId',
           ignore: FieldIgnore.always,
         },
@@ -529,7 +534,7 @@ export default (): DataSetProps => {
           label: intl.get('hivp.invoicesArchiveUpload.view.documentNumber').d('单据编号'),
           type: FieldType.object,
           lovCode: 'HTC.DOCUMENT_CODE_LOV',
-          multiple: ',',
+          multiple: true,
           computedProps: {
             lovPara: ({ record }) => {
               return {
@@ -546,11 +551,13 @@ export default (): DataSetProps => {
         {
           name: 'documentNumber',
           type: FieldType.string,
+          multiple: ',',
           bind: 'documentNumberObj.documentNumber',
         },
         {
           name: 'detailId',
           type: FieldType.string,
+          multiple: ',',
           bind: 'documentNumberObj.detailId',
         },
         {
